e6eb0db6d5e7acff1cfe8bef9bed00eeb0cac69d
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-04-18 19:07:03 +0200

    added middleware in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducers from './contacts-reducers';
 
@@ -15,6 +24,13 @@ export const store = configureStore({
   reducer: {
     contacts: persisteContactReducer,
   },
+
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
