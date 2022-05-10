4a2109de36371e34b5531f54c6957399c9e3320d
Bohdan Strilets <bohdan.strilets@gmail.com>
2022-05-10 12:35:13 +0200

    added basename for browser router

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,10 +1,12 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-  padding-bottom: 30px;
-  border-bottom: 1px solid var(--second-bg-color);
+    padding-bottom: 30px;
+    border-bottom: 1px solid var(--second-bg-color);
+  }
 `;
